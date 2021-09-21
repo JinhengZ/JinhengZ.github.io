79a057fcd2c2855d40e6bab0e1ed58b5970fbc9d
JinhengZ <50747777+JinhengZ@users.noreply.github.com>
2021-09-21 10:48:16 -0400

    add contact me form

---
+++ src/EmailJS.js
@@ -0,0 +1,21 @@
+(function() {
+    emailjs.init("user_XFdzTdNqw9XMEvhvx7UHP");
+})();
+
+function sendmail(){
+    let lName = document.getElementById('lname').value;
+    let fName = document.getElementById("fname").value;
+    let phoneNumber = document.getElementById('phoneNumber').value;
+    let emailAddress = document.getElementById('emailAddress').value;
+    let userMessage = document.getElementById('message').value;
+
+        var contactParams = {
+            first_name: fName,
+            last_name: lName,
+            phone_number: phoneNumber,
+            email_address: emailAddress,
+            message: userMessage
+        };
+
+        emailjs.send('service_p4schuv','template_o9jzx42',contactParams).then(function(res){})
+}
